9f982b3407c00130a60eb73344045697c2a99220
Nick Hale <njohnhale@gmail.com>
2021-08-24 01:16:56 -0400

    init: simple gRPC service skeleton using tonic

---
+++ src/server.rs
@@ -0,0 +1,43 @@
+use tonic::transport::{Server};
+use tonic::{Request, Response, Status};
+
+use snoot::snoot_service_server::{SnootService, SnootServiceServer};
+use snoot::{Condition, Snoot};
+
+pub mod snoot {
+    // Use a tonic macro to populate this module with generated types
+    tonic::include_proto!("snoot"); // Must match the proto package name
+}
+
+#[derive(Debug, Default)]
+pub struct StaticSnootService{}
+
+#[tonic::async_trait]
+impl SnootService for StaticSnootService {
+    async fn boop(
+        &self,
+        request: Request<Snoot>, // Take Request of type Snoot
+    ) -> Result<Response<Condition>, Status> { // Return a Response of type Condition
+        println!("Boop = {:?}", request);
+
+        let condition = snoot::Condition {
+            snoot: request.into_inner().into(),
+            booped: 8,
+        };
+
+        Ok(Response::new(condition))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let snoot_service = StaticSnootService::default();
+
+    Server::builder()
+        .add_service(SnootServiceServer::new(snoot_service))
+        .serve(addr)
+        .await?;
+
+    Ok(())
+}
